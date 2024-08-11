01f8d8ec89e9b86ce63ed2e567bf1529fd38ad3d
Luan Gjokaj <luan@riangle.com>
2024-08-11 11:29:09 +0200

    feat: change meta description

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,5 +1,5 @@
 export const TITLE = "VIDEOJOBS";
 export const PAGE_DESCRIPTION =
-  "Elevate your recruitment with our cutting-edge platform, transforms job postings into engaging, multimedia experiences. the days of dull, text-heavy ads – our solution empowers you to create job listings that truly stand out and captivate top talent.";
+  "Discover the features and tips that will help you create the best job ad experience on the internet.";
 export const OG_IMAGE =
   "https://zdwbgxmxgxblvbwcncdn.supabase.co/storage/v1/object/public/company-data/demo/meta/docs-preview.jpg";
